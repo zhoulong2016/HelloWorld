1fbb7fc2552b9708f64f7e4da3a666c223fc948c
周龙 <long675796382@163.com>
2017-09-18 23:44:05 +0800

    add
    add

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,15 @@
+package main.java;
+
+/**
+ * Created by zhoulong on 2017/9/18.
+ */
+public class HelloWorld {
+
+    public static void main (String[] args){
+
+        System.out.println("HelloWorld");
+
+
+
+    }
+}
