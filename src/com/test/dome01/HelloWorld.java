838627ba31fe0679d8ff24a8e6cf1357267c5fe6
zhoulong <zhoulong@Ctrip.com>
2017-09-19 09:52:41 +0800

    add

---
+++ src/com/test/dome01/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.test.dome01;
+
+/***
+ * HelloWorld 程序
+ * @author zhoulong
+ * @version 1.0
+ */
+public class HelloWorld {
+    /***
+     * 主方法
+     * 打印Helloworld
+     * @param args 主函数输入框
+     */
+
+    public static void main (String[] args){
+
+        System.out.println("HelloWorld");//打印字符串
+
+    }
+}
